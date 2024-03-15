9101358fcbb80a37a1c506050cf8f90d851eab04
harley-zhang <harleyzgolego@gmail.com>
2024-03-15 12:28:06 -0400

    updated chat section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { FaBookmark } from "react-icons/fa6";
 import { LuBrainCircuit } from "react-icons/lu";
 import { FiMessageSquare } from "react-icons/fi";
 import { GoHomeFill } from "react-icons/go";
+import { IoSend } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
+
 
 import './App.css';
 
@@ -15,10 +18,10 @@ function App() {
             <LuBrainCircuit size={35} className="logo" />
             <span className="brand">LumosAI</span>
           </div>
-          <button className="midBtn"><FaPlus size={25} className="plus" /><span className="addBtn" >New Chat</span></button>
+          <button className="midBtn"><FaPlus size={25} className="plusIcon" /><span className="addBtn" >New Chat</span></button>
           <div className="upperSideBottom">
-            <button className="query"><FiMessageSquare size={35} className="messageSquare" />What are the ingredients for a chicken parm?</button>
-            <button className="query"><FiMessageSquare size={35} className="messageSquare" />hello</button>
+            <button className="query"><FiMessageSquare size={35} className="messageIcon" />What are the ingredients for a chicken parm?</button>
+            <button className="query"><FiMessageSquare size={35} className="messageIcon" />hello</button>
           </div>
         </div>
         <div className="lowerSide">
@@ -27,6 +30,16 @@ function App() {
         </div>
       </div>
       <div className="main">
+        <div className="chats">
+          <FaUserCircle size={35} className="chatIcon" /><p className="txt">lorem ipsum</p>
+          <LuBrainCircuit size={35} className="chatIcon" /><p className="txt">lorem ipsum</p>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="send a message" /><button className="send"><IoSend size={35} className="sendIcon" /></button>
+          </div>
+          <p>ChatGPT may produce inacr</p>
+        </div>
       </div>
     </div>
   );
